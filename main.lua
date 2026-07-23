c1aa4a301f5b20aa0d53f8a079a76e8412f99fa7
Gozea <rbournet@gmail.com>
2026-07-23 15:52:34 +0200

    my first shader!

--- main.lua
+++ main.lua
@@ -1,17 +1,21 @@
 local GameSystem = require("GameSystem")
 
 function love.load()
-    -- set physics
     GameSystem.startGame()
+    -- load shaders
+    player_shader = love.graphics.newShader("assets/shaders/player_shader.glsl")
 end
 
 function love.update(dt)
     GameSystem:update(dt)
+    player_shader:send("time", love.timer.getTime())
 end
 
 function love.draw()
     if not gameover then
+        love.graphics.setShader(player_shader)
         player:draw()
+        love.graphics.setShader()
     end
     for k, shot in pairs(shots) do
         shot:draw()
